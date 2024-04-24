77208357dd0906032784d874b0ae54cf9858a3c5
mairascomparim <maira2012scomparim@gmail.com>
2024-04-23 22:24:09 -0300

    feat: Input component2

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,19 +1,14 @@
 import { Button } from "../../../components/common/Button";
 import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
+import styles from "./Shelter.module.css"
 
 
 export function Shelter(){
 
-    function submit(event){
-        event.preventDefault()
-        console.log('submit')
-    }
-
     return (
     <Panel>
-    <form className={styles.container}
-    onSubmit={submit}>
+    <form className={styles.container}>
         <Input label="Nome"/>
         <Button type="submit" >Salvar dados</Button>
     </form>
