4f13136d3fd009184b6c63b133145891830b5541
mairascomparim <maira2012scomparim@gmail.com>
2024-04-23 22:15:54 -0300

    feat: Input component

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,5 +1,22 @@
+import { Button } from "../../../components/common/Button";
+import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 
+
 export function Shelter(){
-    return <Panel>Formulário de abrigo</Panel>
+
+    function submit(event){
+        event.preventDefault()
+        console.log('submit')
+    }
+
+    return (
+    <Panel>
+    <form className={styles.container}
+    onSubmit={submit}>
+        <Input label="Nome"/>
+        <Button type="submit" >Salvar dados</Button>
+    </form>
+    </Panel>
+    )
 }
