078413bfa9d47a7c3f5bfdfbf53b83e1955f34fb
mairascomparim <maira2012scomparim@gmail.com>
2024-04-24 22:16:34 -0300

    feat: Salvando abrigo no banco

--- src/Interfaces/shelter.ts
+++ src/Interfaces/shelter.ts
@@ -1,6 +1,17 @@
+import { string } from "zod"
+
 export interface IShelter{
     shelterName: string
     shelterEmail: string
     shelterPhone: string
     shelterWhatsApp: string
 }
+
+export interface IUpdateShelterRequest{
+    name: string
+    email: string 
+    phone: string
+    whatsApp: string
+}
+
+export interface IUpdateShelterResponse extends IUpdateShelterRequest {}
--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -2,16 +2,90 @@ import { Button } from "../../../components/common/Button";
 import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
 import styles from "./Shelter.module.css"
+import {z} from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import {useHookFormMask} from "use-mask-input"
+import { toast } from "sonner";
+import { updateShelter } from "../../../services/shelter/updateShelter";
+import { useForm } from "react-hook-form";
 
+const shelterSchema = z.object({
+    name: z
+    .string()
+    .min(2, 'Nome deve ter no mínimo 2 caracteres.')
+    .max(30, 'Nome deve ter no máximo 30 caracteres.'),
+    email: z.string().email('Campo deve ser um email.'),
+    phone: z.string().refine(value => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >= 10 && digits <= 11
+    }, 'Número deve ter entre 10 e 11 caracteres.'),
+    whatsApp: z.string().refine(value => {
+        const digits = value.replace(/\D/g, '').length
+        return digits >= 10 && digits <= 11
+    }, 'Número deve ter entre 10 e 11 caracteres.'),
+})
+
+type ShelterSchema = z.infer<typeof shelterSchema>
 
 export function Shelter(){
+    const { handleSubmit, register, formState } = useForm<ShelterSchema>({
+        resolver: zodResolver(shelterSchema),
+    })
+
+    const registerWithMask = useHookFormMask(register)
+
+    async function submit({name, email, phone, whatsApp}: ShelterSchema){
+        const toastId =  toast.loading('Salvando dados')
+        
+        try{
+           await updateShelter({
+            name, 
+            email, 
+            phone: phone.replace(/\D/g, ''), 
+            whatsApp: whatsApp.replace(/\D/g, ''),  
+        })
+        toast.success('Dados salvos com sucesso', {
+            id: toastId,
+            closeButton: true,
+        })
+        }catch{
+            toast.error('Não foi possível salvar os dados', {
+                id: toastId,
+                closeButton: true,
+            })
+        }
+    }
 
     return (
-    <Panel>
-    <form className={styles.container}>
-        <Input label="Nome"/>
-        <Button type="submit" >Salvar dados</Button>
-    </form>
-    </Panel>
+        <Panel>
+            <form className={styles.container} onSubmit={handleSubmit(submit)}>
+                <div>
+                    <Input label="Nome" {...register('name')}/>
+                    {formState.errors?.name && (
+                         <p className={styles.formError}> {formState.errors.name.message}</p>
+                    )}
+                </div>
+                <div>
+                    <Input label="Email" {...register('email')}/>
+                    {formState.errors?.email && (
+                         <p className={styles.formError}> {formState.errors.email.message}</p>
+                    )}
+                </div>
+                <div>
+                    <Input label="Telefone" {...registerWithMask('phone', ['99 9999-99999'])}/>
+                    {formState.errors?.phone && (
+                         <p className={styles.formError}> {formState.errors.phone.message}</p>
+                    )}
+                </div>
+                <div>
+                    <Input label="WhatsApp" {...registerWithMask('whatsApp', ['99 9999-99999'])}/>
+                    {formState.errors?.whatsApp && (
+                         <p className={styles.formError}> {formState.errors.whatsApp.message}</p>
+                    )}
+                </div>
+                <Button type="submit" >Salvar dados</Button>
+            </form>
+        </Panel>
     )
 }
+
